629186c8aa5493b25e41b01e6579104df504fb36
zaymovskey <zaymovskey@mail.com>
2024-06-30 15:20:35 +0300

    init, stylelint, prettier, eslint

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import '@/app/styles/index.scss';
+
+function App() {
+    return (
+        <div className="app">
+            <div className="content-page"></div>
+        </div>
+    );
+}
+
+export default App;
