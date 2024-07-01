ebf8bd5ebbe759abfceb735f092f643813d8a80e
zaymovskey <zaymovskey@mail.com>
2024-07-02 00:01:13 +0300

    remove apollo

--- src/entity/repo/api/index.ts
+++
@@ -1,4 +0,0 @@
-import { useGetRepoQuery } from '@/entity/repo/api/getRepo/getRepo.gen.ts';
-import { useGetReposListQuery } from '@/entity/repo/api/getReposList/getReposList.gen.ts';
-
-export default { useGetRepoQuery, useGetReposListQuery };
--- src/main.tsx
+++ src/main.tsx
@@ -2,24 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app/App.tsx';
 import { BrowserRouter } from 'react-router-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-
-const apolloClient = new ApolloClient({
-    uri: 'https://api.github.com/graphql',
-    headers: {
-        Authorization: `Bearer ${__GITHUB_KEY__}`,
-        'User-Agent': 'Github-Repos',
-    },
-    cache: new InMemoryCache(),
-    connectToDevTools: true,
-});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <BrowserRouter>
-            <ApolloProvider client={apolloClient}>
-                <App />
-            </ApolloProvider>
+            <App />
         </BrowserRouter>
     </React.StrictMode>,
 );
